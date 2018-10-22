7e8eb547ab5ae7f2657f1f5c6a0156ce3e72d05f
Unknown <carlossas_97@hotmail.com>
2018-10-22 15:02:49 -0500

    delete mdb bootstrap module of page.module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,4 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { MDBBootstrapModule } from  'angular-bootstrap-md';
 
 // MODULO NECESARIO
 import { FormsModule } from '@angular/forms';
@@ -28,8 +27,7 @@ import { HomeComponent } from './home/home.component';
          HomeComponent,
      ],
      imports: [
-        MDBBootstrapModule.forRoot(),
-        BrowserModule,
+         BrowserModule,
          SharedModule,
          CommonModule,
          PAGES_ROUTES,
